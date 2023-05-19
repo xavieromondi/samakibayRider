947779dba1faab2f985a04a526e9c81adcbed38a
xavier <omondix214@gmail.com>
2023-05-19 19:54:08 +0300

    hope

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import * as Notifications from "expo-notifications";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+export default function PushTokenGenerator() {
+  const [expoPushToken, setExpoPushToken] = useState(null);
+
+  useEffect(() => {
+    const registerForPushNotificationsAsync = async () => {
+      try {
+        const { status } = await Notifications.requestPermissionsAsync();
+
+        if (status !== "granted") {
+          alert("Failed to get push token for push notification!");
+          return;
+        }
+
+        const expoPushToken = (await Notifications.getExpoPushTokenAsync())
+          .data;
+        console.log("Expo Push Token:", expoPushToken);
+
+        setExpoPushToken(expoPushToken);
+      } catch (error) {
+        console.log("Error getting push token:", error);
+      }
+    };
+
+    registerForPushNotificationsAsync();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {expoPushToken ? (
+        <Text>Your Expo Push Token: {expoPushToken}</Text>
+      ) : (
+        <Text>Loading...</Text>
+      )}
       <StatusBar style="auto" />
     </View>
   );
