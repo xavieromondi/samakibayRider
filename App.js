8f46d6a33a973c84cd4992aa55b9b87a151ad2ae
xavier <omondix214@gmail.com>
2023-05-19 12:04:49 +0300

    app

--- App.js
+++ App.js
@@ -1,49 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import * as Notifications from "expo-notifications";
 import { StyleSheet, Text, View } from "react-native";
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: true,
-  }),
-});
-
-export default function PushTokenGenerator() {
-  const [expoPushToken, setExpoPushToken] = useState(null);
-
-  useEffect(() => {
-    const registerForPushNotificationsAsync = async () => {
-      try {
-        const { status } = await Notifications.requestPermissionsAsync();
-
-        if (status !== "granted") {
-          alert("Failed to get push token for push notification!");
-          return;
-        }
-
-        const expoPushToken = (await Notifications.getDevicePushTokenAsync())
-          .data;
-        console.log("Expo Push Token:", expoPushToken);
-
-        setExpoPushToken(expoPushToken);
-      } catch (error) {
-        console.log("Error getting push token:", error);
-      }
-    };
-
-    registerForPushNotificationsAsync();
-  }, []);
-
+export default function App() {
   return (
     <View style={styles.container}>
-      {expoPushToken ? (
-        <Text>Your Expo Push Token: {expoPushToken}</Text>
-      ) : (
-        <Text>Loading...</Text>
-      )}
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
